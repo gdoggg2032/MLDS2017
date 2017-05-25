62d2cb5158dc0f0c1e38f33c19e4d09270c02a06
Chia-Wen Cheng <chiawen@csie.ntu.edu.tw>
2017-05-25 14:59:36 +0800

    change model to dcgan for 60 epochs

--- hw3/run.sh
+++ hw3/run.sh
@@ -2,7 +2,7 @@ TEST_TEXT=$1
 
 unzip model.zip
 python3 image_generation.py\
- --mode 1 --batch_size 32\
+ --mode 1 --batch_size 256\
   --z_type normal --log model\
    --seed 123 --vocab ./vocab\
-    --model_type dclsgan --test_text $TEST_TEXT
+    --model_type dcgan --test_text $TEST_TEXT
